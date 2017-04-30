61beeb88c1adbcf2421486a66bb0e01e983ae3b8
Yi Zheng <yi.zheng2@rhsmith.umd.edu>
2017-04-30 15:36:28 -0400

    cleaning-1

--- ReadandCleanData.R
+++ ReadandCleanData.R
@@ -15,7 +15,32 @@ investment<-read.csv(file="C:/Users/willi/Desktop/Github/DMproject/investments.c
 download.file(url="https://raw.githubusercontent.com/notpeter/crunchbase-data/master/rounds.csv",
               destfile = "C:/Users/willi/Desktop/Github/DMproject/rounds.csv")
 round<-read.csv(file="C:/Users/willi/Desktop/Github/DMproject/rounds.csv",header=T)
+
 #variable names
 variable<-c(names(acqusition),names(addtion),names(company),names(investment),names(round))
 variable<-unique(variable)
 variable
+#==============================================previous part is the sup data set===============================
+#==============================================below part is the main data set==================================
+#Cruchbase Database
+library(stringr)
+library(dplyr)
+library(data.table)
+library(ggplot2)
+#read data
+download.file(url="https://raw.githubusercontent.com/233hehe/DMproject/master/crunchbase.csv",
+              destfile = "c.csv")
+company=read.csv("c.csv",stringsAsFactors = FALSE,na.strings=c("NA","NaN"))
+#delete useless column
+c1=company
+c1$Company.Name.URL=NULL
+c1$X=NULL
+#Convert to Correct Date format
+c2<-c1
+c2$Status<-as.factor(c2$Status)
+c2$Closed.Date=as.Date(c2$Closed.Date,format = "%m/%d/%Y")
+c2$Founded.Date=as.Date(c2$Founded.Date,format="%m/%d/%Y")
+c2$Number.of.Articles=as.numeric(gsub(x=c2$Number.of.Articles,pattern = ",",replacement = ""))
+
+#
+c2$Company.Length=ifelse(is.na(c2$Closed.Date),Sys.Date()-c2$Founded.Date,c2$Closed.Date-c2$Founded.Date)/365
